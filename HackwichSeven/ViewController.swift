b17e6d919bf8e8b5dd4e8675de477f56c6b87386
CM Student <cmstudent@UHWO-ACM-C-01.local>
2019-04-04 10:47:26 -1000

    3/4/2019 need to finish part 8 , got lost

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapview: MKMapView!
+  
+    let initialLoaction = CLLocation (latitude: 21.361888 , longitude : -158.055725)
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+ 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
+    centerMapOnLocation(location: initialLoaction)
+        
     }
-
+    func centerMapOnLocation (location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance( location.coordinate,regionRadius, regionRadius)
+        mapview.setRegion (coordinateRegion, animated : true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
